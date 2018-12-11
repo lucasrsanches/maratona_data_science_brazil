74878e268ac871f15d6194d2b886a8cba8efd601
lucasrsanches <lucasrodriguessanches@hotmail.com>
2018-12-11 20:11:56 -0200

    Listas: conceitos básicos

---
+++ Semana 01/4-Listas e estruturas de repetição/Listas.py
@@ -0,0 +1,13 @@
+# Criar uma lista
+L = [1, 3, 5, 7, 5.6, "número"]
+
+# Adicionando último valor da lista
+L.append(10)
+
+# Acessar elementos da lista
+L[0] = 100
+print(L)
+print(L[-1])
+
+# Deletar elementos da lista
+del L[1]
