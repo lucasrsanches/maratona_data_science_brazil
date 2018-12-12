0badd67465059d538bf0d6de5ea96433b87c6f7d
lucasrsanches <lucasrodriguessanches@hotmail.com>
2018-12-12 21:44:47 -0200

    Estruturas de repetição

---
+++ Semana 01/4-Listas e estruturas de repetição/Repetição.py
@@ -0,0 +1,18 @@
+V = 0
+
+print("while")
+while V < 5:
+    print(V)
+    V = V + 1
+
+print("--------")
+
+print("for")
+for i in range(0, 7, 3):
+    print(i)
+
+print("--------")
+
+print("Range")
+R = range(10)
+print(list(R))
