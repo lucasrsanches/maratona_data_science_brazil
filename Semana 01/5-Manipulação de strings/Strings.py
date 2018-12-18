dd86abb9a4a5777d4bac0a20357f0086c51be242
lucasrsanches <lucasrodriguessanches@hotmail.com>
2018-12-17 23:20:57 -0200

    Strings

---
+++ Semana 01/5-Manipulação de strings/Strings.py
@@ -0,0 +1,16 @@
+S = 'palavra ou frase'
+
+# Tamanho
+print(len(S))
+
+# Troca
+print(S.replace('palavra', 'outra palavra'))
+
+# Split
+print(S.split('a'))
+
+# Contém
+print('a' in S)
+
+for c in S:
+    print(c)
