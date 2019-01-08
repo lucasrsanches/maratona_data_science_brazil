8d3e914e1431f6fb203a7e4d62da495503460562
lucasrsanches <lucasrodriguessanches@hotmail.com>
2019-01-07 22:41:52 -0200

    Input

---
+++ Semana 01/O poderoso Phyton/2- Entrada, saída, variáveis e operações matemáticas/Input.py
@@ -0,0 +1,9 @@
+print("Informe os seguintes dados: ")
+
+nome = input('Nome: ')
+idade = input('Idade: ')
+altura = input('altura: ')
+
+print(nome, 'tem', idade, 'anos e', altura, 'de altura')
+# Input sempre retorna o tipo str
+print(type(nome), type(idade), type(altura))
