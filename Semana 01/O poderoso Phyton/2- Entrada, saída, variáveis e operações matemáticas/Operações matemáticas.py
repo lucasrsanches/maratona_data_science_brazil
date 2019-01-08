5b4ea8e77717f20986bb43d2cdf9689b1b152e6a
lucasrsanches <lucasrodriguessanches@hotmail.com>
2019-01-07 22:41:41 -0200

    Operações matemáticas

---
+++ Semana 01/O poderoso Phyton/2- Entrada, saída, variáveis e operações matemáticas/Operações matemáticas.py
@@ -0,0 +1,12 @@
+n1 = 27
+n2 = 53
+n3 = 2.5
+resultado = (n1 + n2)/n3 * 8
+
+print(resultado)
+''' + = soma
+ - = subtração
+ * =  multiplicação
+ / = divisão
+ ** = elevado ao quadrado
+ ** (1/2) = raiz quadrada'''
