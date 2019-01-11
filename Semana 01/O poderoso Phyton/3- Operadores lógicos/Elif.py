418d171fcaf8f614c2ae1740036c2dd4174ab42b
lucasrsanches <lucasrodriguessanches@hotmail.com>
2019-01-10 22:01:34 -0200

    Elif

---
+++ Semana 01/O poderoso Phyton/3- Operadores lógicos/Elif.py
@@ -0,0 +1,12 @@
+print('Opções: \n1 = Escreve Hello\n2 = Escreve Hi\n3 = Escreve Yo\n')
+
+opcao = input('Escolha uma opção:')
+
+if opcao == '1':
+    print('Hello')
+elif opcao == '2':
+    print('Hi')
+elif opcao == '3':
+    print('Yo')
+else:
+    print('Opção invalida')
