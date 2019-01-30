fb5e10375c3a1ad3ab752ca8b4d20ff633011823
lucasrsanches <lucasrodriguessanches@hotmail.com>
2019-01-30 19:59:46 -0200

    For

--- Semana 01/O poderoso Phyton/5- Estruturas de laço (For e While)/For.py
+++ Semana 01/O poderoso Phyton/5- Estruturas de laço (For e While)/For.py
@@ -6,7 +6,7 @@ nomes = ['Guilherme', 'Marcelo', 'Joao', 'Julia']
 '''for item in range(4): #range= lista, porém pode ocorrer erro se o tamanho for maior que a variavel
     print(nomes[item])'''
 
-for x in range(len(nomes)):  # len= usa o tamanho exato da lista
+for x in range(len(nomes)):  # len= usa o tamanho exato da lista e transforma em inteiro
     print(nomes[x])
 
 palavra = 'Lucas'
